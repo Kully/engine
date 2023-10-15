41e48c7ac91e4f1c3354972902e564f0d8eff6cf
Adam Kulidjian <adam.kulidjian@gmail.com>
2023-10-16 01:55:23 +0200

    perf: significantly speed up level drawing
    This commit boosts the way we write to the canvas. Rather than iterating through every single pixel on the canvas, we preload sprites into an invisible canvas, then each frame iterate across tiles (larger NxN pixel spaces) and populate with said sprites.
    
    The game was barely able to run at 60 FPS, and now runs very smoothly with only mild perturbances for each frame (4-7ms losses per frame).
    
    Lastly, a few other clean ups were attended too, including starting a helpers.js function for organization, changing the test level a little, and deleting a constant we don't need.

--- src/script.js
+++ src/script.js
@@ -9,6 +9,7 @@ import {
     WALK_CYCLE,
     JUMP_CYCLE,
     SCALE,
+    SCALE2,
     COLOR_ARRAY,
 } from "./data.js";
 
@@ -24,21 +25,10 @@ import {
     COLORS,
 } from "./constants.js";
 
+import {
+    hexToRgb
+} from "./helpers.js";
 
-function validatePixelColor(color, COLOR_ARRAY)
-{
-    if(color.toString().startsWith("#"))
-        return color;
-    else
-        return COLOR_ARRAY[color];
-}
-
-function getValueFrom2DArray(array_2d, x, y)
-{
-    if(x < 0 || x >= array_2d[0].length || y < 0 || y >= array_2d.length)
-        return undefined;
-    return array_2d[y][x];
-}
 
 document.addEventListener("keydown", function(e) {
     for(let key of VALID_CONTROLLER_KEYS)
@@ -62,8 +52,8 @@ document.addEventListener("keyup", function(e) {
     {
         CONTROLLER["lastLeftOrRight"] = e.key;
     }
-
 });
+
 
 // init canvas for background layer
 const canvas = document.getElementById("canvas");
@@ -77,75 +67,121 @@ const ctx2 = canvas2.getContext("2d");
 canvas2.width = canvas.width;
 canvas2.height = canvas.height;
 
+// init canvas that holds sprite data to easily transfer
+const canvasSprites = document.getElementById("canvas-sprites");
+const ctxSprites = canvasSprites.getContext("2d", {willReadFrequently: true});
+
+let levelSpriteCount = Object.keys(SPRITE_LOOKUP).length
+canvasSprites.width = SCALE2 * SCALE * SPRITE_WIDTH * levelSpriteCount
+canvasSprites.height = SCALE2 * SCALE * SPRITE_WIDTH;
+
+
+let spriteSlotLookup = {
+    4: 0,
+    8: 1,
+    5: 2,
+    7: 3,
+    0: 4,
+}
+for(let ptr in spriteSlotLookup)
+    saveSpriteToHiddenCanvas(ptr, SCALE2*SCALE, spriteSlotLookup[ptr]);
+
+
 // player parameters
 const playerScale = SCALE;
 const maxSpeed = SCALE;
 const accInc = SCALE;
 const decInc = SCALE;
 
+function validatePixelColor(color, COLOR_ARRAY)
+{
+    if(color.toString().startsWith("#"))
+        return color;
+    else
+        return COLOR_ARRAY[color];
+}
+
+function getValueFrom2DArray(array_2d, x, y)
+{
+    if(x < 0 || x >= array_2d[0].length || y < 0 || y >= array_2d.length)
+        return undefined;
+    return array_2d[y][x];
+}
+
 function drawLevel()
 {
-    for(let x=0; x<canvas.width; x+=1)
-    for(let y=0; y<canvas.height; y+=1)
+    let xTiles = canvas.width/(SPRITE_WIDTH*SCALE*SCALE2);
+    let yTiles = canvas.height/(SPRITE_WIDTH*SCALE*SCALE2);
+    for(let x=0; x<xTiles+1; x+=1)
+    for(let y=0; y<yTiles+1; y+=1)
     {
-        // deterine which sprite block we are drawing
-        let jump_in_x = Math.floor((CAMERA["xOffset"] + x) / GRID_WIDTH_PX);
-        let jump_in_y = Math.floor((CAMERA["yOffset"] + y) / GRID_WIDTH_PX);
-        let sprite_x = CAMERA["gridXIndex"] + jump_in_x;
-        let sprite_y = CAMERA["gridYIndex"] + jump_in_y;
-        let sprite_ptr = getValueFrom2DArray(LEVEL, sprite_x, sprite_y);
-
-        // draw pixel
-        let pixelColor;
-        if(sprite_ptr === undefined || sprite_ptr === null)
-        {
-            pixelColor = COLORS["undefined"];
-        }
-        else
-        {
-            let color_x = Math.floor( ((x+CAMERA["xOffset"])%GRID_WIDTH_PX) / (GRID_WIDTH_PX / SPRITE_WIDTH) );
-            let color_y = Math.floor( ((y+CAMERA["yOffset"])%GRID_WIDTH_PX) / (GRID_WIDTH_PX / SPRITE_WIDTH) );
-            let color_idx = color_y * SPRITE_WIDTH + color_x;
-
-            pixelColor = SPRITE_LOOKUP[sprite_ptr]["sprite"][color_idx];
-            pixelColor = validatePixelColor(pixelColor, COLOR_ARRAY);
+        let spritePtr = getValueFrom2DArray(LEVEL, x, y)
+        if(spritePtr === undefined) {
+            spritePtr = 0;
         }
 
-        if((pixelColor.endsWith("FF") && pixelColor.length === 9) || pixelColor.length === 7)
-        {
-            ctx.fillStyle = pixelColor;
-            ctx.fillRect(x, y, 1, 1);
-        }
+        let savedData = getSpriteFromHiddenCanvas(spritePtr);
+        let tileX = x;
+        let tileY = y;
+        ctx.putImageData(
+            savedData,
+            -1 * CAMERA["xOffset"] + tileX * (SCALE2 * SCALE * SPRITE_WIDTH),
+            -1 * CAMERA["yOffset"] + tileY * SCALE * SCALE2 * SPRITE_WIDTH,
+        );
     }
 }
 
+function saveSpriteToHiddenCanvas(spritePtr, scale, slotX)
+{
+    let spriteData = SPRITE_LOOKUP[spritePtr]["sprite"];
+    for(let i=0; i < spriteData.length; i+=1)
+    {
+        let imageData = ctxSprites.createImageData(2*SCALE, 2*SCALE);
+        let colorPtr = spriteData[i];
+        let hex = COLOR_ARRAY[colorPtr];
+        let rgbArray = hexToRgb(hex);
+
+        for(let j=0; j<SPRITE_WIDTH*SPRITE_WIDTH; j+=1)
+        {
+            imageData.data[4*j + 0] = rgbArray[0];
+            imageData.data[4*j + 1] = rgbArray[1];
+            imageData.data[4*j + 2] = rgbArray[2];
+            imageData.data[4*j + 3] = rgbArray[3];
+        }
+        let x = SCALE*SPRITE_WIDTH*slotX + SCALE*(i % SPRITE_WIDTH);
+        let y = 0 + SCALE*(Math.floor(i / SPRITE_WIDTH));
+        ctxSprites.putImageData(imageData, SCALE2*x, SCALE2*y);
+    }
+}
+
+function getSpriteFromHiddenCanvas(spritePtr)
+{
+    let slotX = spriteSlotLookup[spritePtr]
+
+    let left = SCALE2 * slotX*SCALE*SPRITE_WIDTH;
+    let top = 0*SCALE*SPRITE_WIDTH;
+    let width = SCALE2*SCALE*SPRITE_WIDTH;
+    let height = SCALE2*SCALE*SPRITE_WIDTH;
+    let savedData = ctxSprites.getImageData(left, top, width, height);
+    return savedData;
+}
 
 function updatePlayerSpeed()
 {
     if(CONTROLLER["ArrowLeft"] === 1 && CONTROLLER["ArrowRight"] === 0)
-    {
         PLAYER["speed"] -= accInc;
-    }
     else
     if(CONTROLLER["ArrowLeft"] === 0 && CONTROLLER["ArrowRight"] === 1)
-    {
         PLAYER["speed"] += accInc;
-    }
     else
     {
         if(PLAYER["speed"] > 0.4)
-        {
             PLAYER["speed"] -= decInc;
-        }
         else
         if(PLAYER["speed"] < -0.4)
-        {
             PLAYER["speed"] += decInc;
-        }
         else
-        {
             PLAYER["speed"] = 0;
-        }
     }
 
     // throttle the speed
@@ -239,9 +275,9 @@ function findAnimationCycle()
 
 function gameLoop(e)
 {
-
     CAMERA["xOffset"] += CAMERA["velocityX"];
     PLAYER["x"] -= CAMERA["velocityX"];
+
     drawLevel();
 
     updatePlayerSpeed();
