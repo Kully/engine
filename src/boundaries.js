310540e8a74a5560a68fe3a0b1eceb49564b8289
Adam Kulidjian <adam.kulidjian@gmail.com>
2024-03-31 18:39:20 +0200

    feat(collision): smoothly wall eject and standing
    This is a messy commit, since the work is not really done and there is more buggy movement introduced here. Will need to work on these changes.

--- src/boundaries.js
+++ src/boundaries.js
@@ -31,23 +31,34 @@ export function isPlayerStanding(level) {
 	// Unexpected results if 0.5 or more.
 	// The bigger the number, the further off the
 	// edge the player can stand before falling.
-	const STAND_LENIANCY = 0.25;
+	// const STAND_LENIANCY = 0.25;
+	const STAND_LENIANCY = 0;
 
 	let playerGridFloatX = getPlayerGridX();
 	let playerGridFloatY = getPlayerGridY();
 	let y = Math.floor(playerGridFloatY);
 
+	// let spriteXLeftPtr = getValueFrom2DArray(
+	// 	level,
+	// 	Math.max(Math.round(playerGridFloatX - STAND_LENIANCY), 0),
+	// 	y,
+	// );
+	// let spriteXRightPtr = getValueFrom2DArray(
+	// 	level,
+	// 	Math.max(Math.round(playerGridFloatX + STAND_LENIANCY), 0),
+	// 	y,
+	// );
+
 	let spriteXLeftPtr = getValueFrom2DArray(
 		level,
-		Math.max(Math.round(playerGridFloatX - STAND_LENIANCY), 0),
+		Math.floor(playerGridFloatX),
 		y,
 	);
 	let spriteXRightPtr = getValueFrom2DArray(
 		level,
-		Math.max(Math.round(playerGridFloatX + STAND_LENIANCY), 0),
+		Math.ceil(playerGridFloatX),
 		y,
 	);
-
 	let isBoundaryBelowYou = (
 		SPRITE_LOOKUP[spriteXLeftPtr]["hitbox"] === true ||
 		SPRITE_LOOKUP[spriteXRightPtr]["hitbox"] === true
@@ -72,6 +83,8 @@ export function getSpritePtrPlayerInside(level) {
 
 export function handleBoundaryCollision(level) {
 	// set up the variables
+	let playerGridFloatX = getPlayerGridX();
+	let playerGridFloatY = getPlayerGridY();
 	let playerGridX = PLAYER["x"] / GRID_WIDTH_PX;
 	let playerGridY = PLAYER["y"] / GRID_WIDTH_PX;
 	playerGridX += CAMERA["xOffset"] / GRID_WIDTH_PX;
@@ -79,7 +92,6 @@ export function handleBoundaryCollision(level) {
 
 	let xTileCurrent = Math.round(playerGridX);
 	let yTileCurrent = Math.round(playerGridY);
-	console.log(xTileCurrent);
 
 	let xTileToYourLeft = Math.floor(playerGridX);
 	let xTileToYourRight = Math.ceil(playerGridX);
@@ -87,30 +99,58 @@ export function handleBoundaryCollision(level) {
 	let yTileBelowYou = Math.ceil(playerGridY);
 
 	// deal with boundaries on your left
+	let horizontalEjectionSpeed = 4;
 	let spriteToLeft = level[yTileCurrent - 1][xTileToYourLeft];
 	if (spriteToLeft === undefined || SPRITE_LOOKUP[spriteToLeft]["hitbox"] === true) {
-		PLAYER["x"] = (xTileToYourLeft + 1) * GRID_WIDTH_PX;
-		PLAYER["x"] -= CAMERA["xOffset"];
+		if(playerGridX - xTileToYourLeft < 0.2) {
+			PLAYER["x"] = (xTileToYourLeft + 1) * GRID_WIDTH_PX;
+			PLAYER["x"] -= CAMERA["xOffset"];
+		}
+		else {
+			PLAYER["x"] += horizontalEjectionSpeed;
+		}
 	}
 
 	// deal with boundaries on your right
 	let spiteToRight = level[yTileCurrent - 1][xTileToYourRight];
 	if (spiteToRight === undefined || SPRITE_LOOKUP[spiteToRight]["hitbox"] === true) {
-		PLAYER["x"] = (xTileToYourLeft) * GRID_WIDTH_PX;
-		PLAYER["x"] -= CAMERA["xOffset"];
+		if(playerGridX - xTileToYourLeft < 0.2) {
+			PLAYER["x"] = (xTileToYourLeft) * GRID_WIDTH_PX;
+			PLAYER["x"] -= CAMERA["xOffset"];
+		}
+		else {
+			PLAYER["x"] -= horizontalEjectionSpeed;
+		}
 	}
 
 	// deal with boundaries above you
 	let spriteAboveYou = getValueFrom2DArray(level, xTileCurrent, yTileAboveYou - 1);
 	if (spriteAboveYou === undefined || SPRITE_LOOKUP[spriteAboveYou]["hitbox"] === true) {
+		PLAYER["jumpJuice"] = 0;
 		PLAYER["y"] = (yTileAboveYou + 1) * GRID_WIDTH_PX;
 		PLAYER["y"] -= CAMERA["yOffset"];
 	}
 
 	// deal with boundaries below you
-	let currentSpritePtr = getSpritePtrPlayerInside(level);
-	if (SPRITE_LOOKUP[currentSpritePtr]["hitbox"] === true) {
-		let playerGridFloatY = getPlayerGridY();
-		PLAYER["y"] = Math.floor(playerGridFloatY) * GRID_WIDTH_PX - CAMERA["yOffset"];
-	}
+	let spritePtrXRound = getValueFrom2DArray(
+		level,
+		Math.round(playerGridFloatX),
+		Math.floor(playerGridFloatY),
+	);
+	let spritePtrXFloor = getValueFrom2DArray(
+		level,
+		Math.floor(playerGridFloatX),
+		Math.floor(playerGridFloatY),
+	);
+
+	if (SPRITE_LOOKUP[spritePtrXRound]["hitbox"] === true) {
+		PLAYER["y"] = Math.floor(playerGridFloatY) * GRID_WIDTH_PX;
+		PLAYER["y"] -= CAMERA["yOffset"];
+	};
+
+	// if any part of you touches a boundary while falling, stop falling
+	if (SPRITE_LOOKUP[spritePtrXFloor]["hitbox"] === true && PLAYER["speedY"] >= 0) {
+		PLAYER["y"] = Math.floor(playerGridFloatY) * GRID_WIDTH_PX;
+		PLAYER["y"] -= CAMERA["yOffset"];
+	};
 }
